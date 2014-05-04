f9b73344c49a0215633a07cdd751a48e58b3e1a1
unknown <alexanderyordanov96@gmail.com>
2014-05-04 22:08:49 +0300

    Fixed problem with home page(popular lists)

--- public/app/main/controllers/mainController.js
+++ public/app/main/controllers/mainController.js
@@ -21,23 +21,51 @@ app.controller('MainController', function($scope, CachedPlaylists, identity, Pla
         $scope.playlist = list;
         var current;
         var max1 = {rate:0}, max2 = {rate:0}, max3 = {rate:0};
+        var take1=false,take2=false,take3=false;
         for(var i=0;i<collection.length;i++){
             current = collection[i];
             if(max1.rate < current.rate && current !=list){
                 max1 = current;
+                take1 = true;
             }
+        }
+        if(!take1){
+            max1 = collection[collection.length-1];
+            max2 = collection[collection.length-2];
+            max3 = collection[collection.length-3];
         }
         for(var i=0;i<collection.length;i++){
             current = collection[i];
             if(max2.rate < current.rate && current !=list && current != max1){
                 max2 = current;
+                take2=true;
             }
-        }for(var i=0;i<collection.length;i++){
+            //if(!take2){
+            //    for(var i=0;i<collection.length;i++){
+            //        current = collection[i];
+            //        if(current != max1 && current != list){
+            //            max2=current;
+            //            break;
+            //        }
+            //    }
+            //}
+        }
+        for(var i=0;i<collection.length;i++){
             current = collection[i];
             if(max3.rate < current.rate && current != list && current != max1 && current != max2){
                 max3 = current;
+                take3=true;
             }
         }
+        //if(!take3){
+        //    for(var i=0;i<collection.length;i++){
+        //            current = collection[i];
+        //            if(current != max1 && current != list && current != max2){
+        //                max3=current;
+        //                break;
+        //            }
+        //        }
+        //}
         var popular = [];
         popular.push(list);
         popular.push(max1);
--- server/models/Playlist.js
+++ server/models/Playlist.js
@@ -13,7 +13,7 @@ var Playlist = mongoose.model('Playlist', playlistSchema);
 
 module.exports.seedInitialPlaylist = function(){
     //Playlist.remove({username: 'alex'}, function(){console.log("removed");});
-    //Playlist.remove({title: 'Digital Daggers'}, function(){console.log("removed");});
+    //Playlist.remove({}, function(){console.log("removed");});
     Playlist.find({}).exec(function(err, collection){
         if(err){
             console.log('Cant find playlists: ' + err);
@@ -23,19 +23,19 @@ module.exports.seedInitialPlaylist = function(){
         if(collection.length == 0){
             Playlist.create({username: 'alex', title: 'Simple plan', published: new Date('2/5/2014'),
                  videos: [{title: 'Your love is a lie', id: 'XAbcgmwq3EU'},
-                            {title: 'Astronaut', id: 'N-MgRkSh5Xk'}], cover: 'http://simple-plan.sarkisozlerik.com/2014images/s/simple-plan-0.jpg', rate: 0});
+                            {title: 'Astronaut', id: 'N-MgRkSh5Xk'}], cover: 'http://simple-plan.sarkisozlerik.com/2014images/s/simple-plan-0.jpg', rate: 4});
             Playlist.create({username: 'alex', title: 'Les Friction', published: new Date('2/5/2014'),
                  videos: [{title: 'World On Fire', id: 'uKvx7J7ZqqM'},
-                            {title: 'Louder Than Words', id: 'VGolV-wG9Sk'}], cover: 'http://www.rockcellarmagazine.com/wp-content/uploads/2011/11/lesfriction.jpg', rate: 0});
+                            {title: 'Louder Than Words', id: 'VGolV-wG9Sk'}], cover: 'http://www.rockcellarmagazine.com/wp-content/uploads/2011/11/lesfriction.jpg', rate: 5});
             Playlist.create({username: 'alex', title: 'Sum 41', published: new Date('2/5/2014'),
                  videos: [{title: 'In Too Deep', id: 'emGri7i8Y2Y'},
-                            {title: 'Still Waiting', id: 'qO-mSLxih-c'}], cover: 'http://sum-41.sarkisozlerik.com/2014images/s/sum-41-0.jpg', rate: 0});
+                            {title: 'Still Waiting', id: 'qO-mSLxih-c'}], cover: 'http://sum-41.sarkisozlerik.com/2014images/s/sum-41-0.jpg', rate: 2});
             Playlist.create({username: 'alex', title: '30 Seconds to Mars', published: new Date('2/5/2014'),
                  videos: [{title: 'Night of a Hunter', id: 'zK268TLKCK4'},
-                            {title: 'Stranger in a Strang Land', id: 'RmOIDAa3Dy0'}], cover: 'http://indyconcerts.com/wp-content/uploads/2010/10/30-Seconds-Mars-vr032.jpg', rate: 0});
+                            {title: 'Stranger in a Strang Land', id: 'RmOIDAa3Dy0'}], cover: 'http://indyconcerts.com/wp-content/uploads/2010/10/30-Seconds-Mars-vr032.jpg', rate: 1});
             Playlist.create({username: 'alex', title: 'Linkin Park', published: new Date('2/5/2014'),
                  videos: [{title: 'Numb', id: 'kXYiU_JCYtU'},
-                            {title: 'Breaking the Habit', id: 'v2H4l9RpkwM'}], cover: 'http://images1.fanpop.com/images/image_uploads/LP-linkin-park-883001_1280_1024.jpg', rate: 0});
+                            {title: 'Breaking the Habit', id: 'v2H4l9RpkwM'}], cover: 'http://images1.fanpop.com/images/image_uploads/LP-linkin-park-883001_1280_1024.jpg', rate: 3});
 
             console.log("Playlists added...");
         }
